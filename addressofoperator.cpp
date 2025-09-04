0a704b318e01b57ab83b84fb17708b542d5593cb
Rahul79090 <kumarrahul032004@gmail.com>
2025-09-04 10:34:24 +0530

    addressofaperato

---
+++ addressofoperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=5;
+
+    cout<<num<< endl;
+
+    //address of opertator
+    cout<<"address of num is :-"<<&num<<endl;
+
+    return 0;
+
+    
+}
