3d7991f03aa65ffc13983bbc92b134b41ffdc7ec
Rahul79090 <kumarrahul032004@gmail.com>
2025-12-14 23:07:22 +0530

    hms.cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+void login()
+// {
+//     string password;
+
+//     cout << "\n=====================================\n";
+//     cout << "              LOGIN\n";
+//     cout << "=====================================\n";
+//     cout << "Enter Password: ";
+//     cin >> password;
+
+//     if (password != "pass")
+//     {
+//         cout << "Wrong password! Try again.\n";
+//         login();
+//     }
+//     else
+//     {
+//         cout << "Access Granted!\n";
+//     }
+// }
